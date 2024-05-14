2557d011df506c2b290eba8026a9e29bffb4bb8d
dami2004 <d.emelianoff@gmail.com>
2024-05-14 17:06:03 +0300

    sma

--- SMA/lesson_3.py
+++ SMA/lesson_3.py
@@ -1,27 +1,24 @@
 import json
 
-# Загрузка данных из файла
+
 def load_data(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
     return data
 
-# Пример функции для предсказания меток NER
+
 def predict_ner(sentences):
     predictions = []
     for sentence in sentences:
         for token in sentence:
-            # Ваш алгоритм предсказания меток NER
-            # В данном примере предполагаем, что все метки равны 'O'
             predictions.append('O')
     return predictions
 
-# Загрузка данных
+
 file_path = './data/Soc_Net_Task_3_File_2.json'
 data = load_data(file_path)
 
-# Получение предсказаний
 predictions = predict_ner(data['sentences'])
 
-# Вывод предсказаний для всех токенов
+
 print(", ".join(predictions))
